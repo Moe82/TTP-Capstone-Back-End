f6d77ff2e8c388b77de77ca3f0332f3a7fb399f7
justinwu1 <justinwu820@gmail.com>
2021-01-29 19:59:53 -0500

    Adding students done

--- api/students.js
+++ api/students.js
@@ -6,33 +6,52 @@ const { Student } = require('../db/models');
 
 // Routes for student
 
-router.get('/', async (req, res, next) => {
-    try{
-         await Student.findAll()
-        .then (
-          response => res.json(response)
-        )
-        
-    }catch(error){
-        console.log(error)
+router.get('/:id', async (req, res, next) => {
+  try {
+    const data = await Student.findAll({
+      where: {
+        CourseId: req.params.id
+      }
+    })
+
+      .then(
+        response => res.json(response)
+      )
+
+  } catch (error) {
+    console.log(error)
+  }
+});
+
+
+// route to create a student
+
+router.post('/',  (req, res, next) => {
+  const arr = req.body.formValues.name.split(",");
+  let data;
+  if (arr.length > 1) {
+    for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i])
+      data =  Student.create({
+        name: arr[i],
+        CourseId: parseInt(req.body.courseId)
+      })
     }
-  });
+  } else {
+    data =  Student.create({
+      name: req.body.formValues.name,
+      CourseId: parseInt(req.body.courseId)
+    })
+  }
+  data 
+    .then(student => res.status(200).json(student))
+    .catch(error => {
+      console.log("There is an error" + error);
+      res.status(500).send('Hey there is an Error' + error)
+    })
 
-
-  // route to create a student
-
-
-  router.post('/', (req, res, next) =>{
-    console.log(req.body)
-    Student.create({
-       name: req.body.name
-     })
-     .then(campus => res.status(200).send("succes"))
-   .catch(error =>{
-     res.status(500).send('Hey there is an Error' + error)
-   })
- })
+})
 
 
 
-  module.exports = router;
+module.exports = router;

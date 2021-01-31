89d86ece97411dfc99bbacdff56c4fed1fd4f682
Mohamed Shafee <mshafee1993@gmail.com>
2021-01-31 10:28:47 -0500

    make it better

--- db/db.js
+++ db/db.js
@@ -1,25 +1,16 @@
 const Sequelize = require('sequelize');
 const { name } = require('../package.json');
 require('dotenv').config()
-// Initialize database with Sequelize
 
-
-const db = new Sequelize( process.env.DATABASE_URL,
-  {
-    development: {
-      url: 'DATABASE_URL',
-      dialect: 'postgres',
-      dialectOptions: {
-          ssl: {
-              require: true,
-              rejectUnauthorized: false
-          }
-      }     
+const database = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      sslmode: 'require',
+      rejectUnauthorized: false
     }
   }
-);
-
-
-
+})
 
 module.exports = db;
